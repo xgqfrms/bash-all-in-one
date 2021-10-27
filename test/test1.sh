de4ac25a42c31c5a610cdb94a7a6ef97a5fa93c3
xgqfrms <xgqfrms@outlook.com>
2021-10-27 11:23:00 +0800

    bash function define

---
+++ test/test1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# function 声明式
+function test {
+  echo "test function with keyword"
+}
+test
---
+++ test/test2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# 命令式 ()
+test (){
+  echo "test function with ()"
+}
+test
